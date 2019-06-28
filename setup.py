79f581d5a3bda1b3ec8c504aa4b4174dab9cdc6a
Junpu Fan <junpu.fan@target.com>
2019-06-28 12:24:57 -0500

    init

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='serial-j',
-      version='0.1',
+      version='0.0.1',
+      author='Junpu Fan',
+      author_email='junpufan@me.com ',
       description=('making python class out of python dictionary, with '
                    'default conversion method to json string or back to '
                    'python dictionary.'),
       url='https://github.com/JunpuFan/serial-j',
-      author='Junpu Fan',
-      author_email='junpufan@me.com ',
       license='MIT',
       packages=['serial-j'],
       zip_safe=False)
